64e00c72e7b4c1bcab0091123824047cea8ec3bc
traveller <2186480198@qq.com>
2025-04-06 15:24:04 +0800

    MD5短链生成器

--- shortLink/src/main/java/cn/edu/hut/wx/controller/ShortController.java
+++ shortLink/src/main/java/cn/edu/hut/wx/controller/ShortController.java
@@ -1,11 +1,15 @@
 package cn.edu.hut.wx.controller;
 
+import cn.hutool.Hutool;
+import cn.hutool.crypto.digest.MD5;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+
+import static javax.swing.text.html.HTML.Tag.BASE;
 
 @RestController
 @RequestMapping("/shortController")
@@ -14,6 +18,7 @@ public class ShortController {
     private static final String SHORT_URL_PREFIX = "http://localhost:8080/shortController/";
     private static final String CHAR_SET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final int SHORT_URL_LENGTH = 6;
+    private static final int BASE = CHAR_SET.length();
     private final Map<String, String> shortToLongMap = new HashMap<>();
     private final Map<String, String> longToShortMap = new HashMap<>();
     private final Random random = new Random();
@@ -49,6 +54,24 @@ public class ShortController {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < SHORT_URL_LENGTH; i++) {
             sb.append(CHAR_SET.charAt(random.nextInt(CHAR_SET.length())));
+        }
+        return sb.toString();
+    }
+    //MD5短链生成器
+    private String generateShortUrlIdByMD5(String longUrl) {
+        MD5 md5 = new MD5();
+        String md5Hash = md5.digestHex(longUrl);
+        // 截取 MD5 哈希值的一部分
+        String subHash = md5Hash.substring(0, 8);
+        long num = Long.parseLong(subHash, 16);
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            sb.append(CHAR_SET.charAt((int) (num % BASE)));
+            num /= BASE;
+        }
+        // 不足指定长度时补零
+        while (sb.length() < SHORT_URL_LENGTH) {
+            sb.insert(0, CHAR_SET.charAt(0));
         }
         return sb.toString();
     }
